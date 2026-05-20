3ebd3fcaa4618ac3555e41fa4d50a5fd16202a10
om shankar <omshankar2016@gmail.com>
2026-05-21 02:27:10 +0530

    algorithm to match template overloads arent as complex as function overloads

--- chapter11/non_type_function_templates/ambiguous.cpp
+++ chapter11/non_type_function_templates/ambiguous.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+
+template <int N>
+void printPositive(){
+  static_assert(N >= 0, "negative value not accepted\n");
+
+  std::cout << N << "\n";
+}
+
+template <char N>
+void printPositive(){
+  static_assert(N >= 0, "negative value not accepted\n");
+
+  std::cout << N << "\n";
+}
+
+int main(){
+  printPositive<5>();
+  // leads to ambiguous as algorithm to match template overloads arent as complex as
+  // algorithm to match function overloads.
+  printPositive<'c'>()
+  // this is also ambiguous
+  return 0;
+}
