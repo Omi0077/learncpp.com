e72b6ac939f5530d01d03ff427015e4a9b3b88bd
om shankar <omshankar2016@gmail.com>
2026-05-21 02:41:19 +0530

    removing deleted source files

--- chapter11/non_type_function_templates/ambiguous.cpp
+++ chapter11/non_type_function_templates/ambiguous.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
-template <int N>
-void printPositive(){
-  static_assert(N >= 0, "negative value not accepted\n");
+// template <int N>
+// void printPositive(){
+//   static_assert(N >= 0, "negative value not accepted\n");
 
-  std::cout << N << "\n";
-}
+//   std::cout << N << "\n";
+// }
 
-template <char N>
+// template <char N>
+// void printPositive(){
+//   static_assert(N >= 0, "negative value not accepted\n");
+
+//   std::cout << N << "\n";
+// }
+
+// one way to overcome this is using auto instead of having different overloads
+
+template <auto N>
 void printPositive(){
   static_assert(N >= 0, "negative value not accepted\n");
 
@@ -15,10 +24,14 @@ void printPositive(){
 }
 
 int main(){
-  printPositive<5>();
+  // printPositive<5>();
   // leads to ambiguous as algorithm to match template overloads arent as complex as
   // algorithm to match function overloads.
-  printPositive<'c'>()
+  // printPositive<'c'>()
   // this is also ambiguous
+
+  // now calling will not lead to ambiguous matches
+  printPositive<5>();
+  printPositive<'A'>();
   return 0;
 }
